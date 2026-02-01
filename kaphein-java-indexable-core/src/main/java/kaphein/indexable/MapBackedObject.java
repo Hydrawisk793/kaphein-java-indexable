d38ec0ec03ea7c04adab6ddf6174ca5f91511984
Hydrawisk793 <hyw793@gmail.com>
2026-02-01 15:26:20 +0900

    fix(wip): Correct the documentation comment of `MapBackedObject#asMap` method.

--- kaphein-java-indexable-core/src/main/java/kaphein/indexable/MapBackedObject.java
+++ kaphein-java-indexable-core/src/main/java/kaphein/indexable/MapBackedObject.java
@@ -8,15 +8,10 @@ import java.util.Map;
 public interface MapBackedObject extends Indexable
 {
   /**
-   * <p>
-   * Returns a {@link Map} accessor for the {@link Indexable}. (optional)
-   * </p>
+   * <p>Returns a {@link Map} accessor for the {@link MapBackedObject}.</p>
+   * <p>Unlike {@link toMap} method, this does not create a copied {@link Map}.</p>
    * 
-   * <p>
-   * Unlike {@link toMap} method, this does not create a copied {@link Map}.
-   * </p>
-   * 
-   * @return A {@link Map} accessor for the {@link Indexable}.
+   * @return A {@link Map} accessor for the {@link MapBackedObject}.
    */
   Map<String, Object> asMap();
 }
